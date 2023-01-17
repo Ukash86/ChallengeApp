9e213c666308b249dd1e405e32bb25394fcc8de3
Uk_ash <ukasek1986@googlemail.com>
2023-01-17 21:21:06 +0000

    praca domowa dzien 4

--- ChallangeApp/ChallangeApp/Program.cs
+++ ChallangeApp/ChallangeApp/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string name = "Ewa";
+string plec = "Kobieta";
+var age = 33;
+string word1 = "Niepełnoletni";
+string word2 = " ";
+string word3 = "Mężczyzna";
+
+if (age > 38)
+{
+    Console.WriteLine("Kobieta powyżej 38 lat");
+}
+else
+{
+    Console.WriteLine("Kobieta poniżej 38 lat");
+}
+if (age <= 33) 
+{
+    Console.WriteLine("Ewa, lat 33");
+}
+string result = word1 + word2 + word3;
+Console.WriteLine(result);
