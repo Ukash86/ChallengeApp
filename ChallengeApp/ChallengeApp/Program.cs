50e9a7d1b142c860b8790750106c89119d72a50a
Uk_ash <ukasek1986@googlemail.com>
2023-01-18 18:20:54 +0000

    dzien 4 (praca domowa)

---
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -0,0 +1,31 @@
+﻿string name = "Ewa";
+char sex = 'K';
+var age = 33;
+
+if (sex == 'K')
+{
+    if (age > 38)
+    {
+        Console.WriteLine("Kobieta powyżej 38 lat");
+    }
+    else if (age < 38)
+    {
+        Console.WriteLine("Kobieta poniżej 38 lat");
+
+        if (age == 33 && name == "Ewa") ;
+    }
+    {
+        Console.WriteLine("Ewa, lat 33");
+    }
+}
+else if (sex != 'K')
+{
+    if (age >= 18)
+    {
+        Console.WriteLine("Pełnoletni Mężczyzna");
+    }
+    else if (age < 18)
+    {
+        Console.WriteLine("Niepełnoletni Mężczyzna");
+    }
+}
