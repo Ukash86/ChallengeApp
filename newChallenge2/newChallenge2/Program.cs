d405a460c8948b218bd4c7103106c7fbacafaf92
Uk_ash <ukasek1986@googlemail.com>
2023-01-21 14:06:15 +0000

    pd d5 v3

---
+++ newChallenge2/newChallenge2/Program.cs
@@ -0,0 +1,19 @@
+﻿int number = 637662969;
+string numberInString = number.ToString();
+char[] digits = numberInString.ToArray();
+
+List<char> numbers = new List<char>();
+string[] name = { "0", "1", "2", "3", "4", "5", "6", "7", "8", "9" };
+
+foreach (var digit in name)
+{
+    int times = 0;
+    foreach (var x in digits)
+    {
+        if (digit == x.ToString())
+        {
+            times++;
+        }
+    }
+    Console.WriteLine(digit + " => " + times);
+}
